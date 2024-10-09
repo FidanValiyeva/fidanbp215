6b62318b6cac7f6ed8fa855022ff22795488a7c4
FidanValiyeva <fidanxv-bp215@code.edu.az>
2024-10-09 11:50:38 +0400

    first commit

---
+++ fidan.cs
@@ -0,0 +1,13 @@
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        int[] numbers = { 4, 8, 97, 46, 67 };
+        int a = Min(numbers);
+        Console.WriteLine(a);
+
+        static int Min(int[] numbers)
+        {
+
+            int minValue = numbers.Min();
+            return minValue;
