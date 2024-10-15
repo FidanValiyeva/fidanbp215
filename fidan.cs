0eae7dd827c98901497c698a552530719e1ce6da
FidanValiyeva <fidanxv-bp215@code.edu.az>
2024-10-15 06:44:34 +0400

    first commit

--- fidan.cs
+++ fidan.cs
@@ -1,13 +1,59 @@
-internal class Program
+public class HelloWorld
 {
-    static void Main(string[] args)
+    static void Main (string[] args) 
     {
-        int[] numbers = { 4, 8, 97, 46, 67 };
-        int a = Min(numbers);
-        Console.WriteLine(a);
+        Car car = new Car ("opel","cruise","1000","4");
+        car.ShowfullInfo();
+        Motorcycle motorcycle = new Motorcycle ("hyperbike","3000","5");
+        motorcycle.ShowfullInfo();
+    }
+   
+}
+   
+public class Vehicle
+{
+    public string brand;
+    public string model;
+    public double mileage;
+ 
+    public  Vehicle (string brand,string model, double mileage)
+    {
+        this.brand = brand;
+        this.model = model;
+        this.mileage = mileage;
+    }
+}
+public class Car : Vehicle
+        
+{ 
+    public int doorsNum;
+    
+    public void ShowfullInfo()
+    {
+        Console.WriteLine($"Car brand: {brand}, model: {Model}, mileage: {mileage}, doors: {doorsNum}");
+    }
 
-        static int Min(int[] numbers)
-        {
 
-            int minValue = numbers.Min();
-            return minValue;
+    public Car(string brand,string model, double mileage,int doorsNum):base(model,mileage)
+    {
+        this.doorsNum = doorsNum;
+    }
+}
+        
+public class Motorcycle:Vehicle
+{
+        
+            
+    public int wheelNum;
+            
+        
+    public  Motorcycle(string brand,string model, double mileage,int wheelNum):base(brand, model,mileage)
+    {
+        this.wheelNum = wheelNum;
+    }
+        
+    public void ShowfullInfo()
+    {
+        Console.WriteLine($"Motorcycle: {brand},model:{model},milage:{milage},wheelNum:{wheelNum}");
+    }
+}
